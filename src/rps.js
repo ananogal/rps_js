a584dc5d27a2eb02b5531bb7a68cf0fa200a9272
ananogal <ana.nogal@gmail.com>
2014-10-15 16:42:24 +0100

    Refactoring the ifs

--- src/rps.js
+++ src/rps.js
@@ -13,14 +13,16 @@ function Draw(){
 	this.type ="Draw";
 };
 
-function Rules(){};
+function Rules(){
+	this.trump = {"Rock": "Scissors",
+				"Paper" : "Rock",
+				"Scissors" : "Paper"}	
+};
 
 Rules.prototype.decidesWinner = function(optionOne, optionTwo) {
 
 	if(optionOne.type === optionTwo.type) return new Draw();
-	if((optionOne.type === 'Rock' && optionTwo.type === 'Scissors') || 
-		(optionOne.type ==='Paper' && optionTwo.type === 'Rock') ||
-		(optionOne.type === 'Scissors' && optionTwo.type === 'Paper'))
+	if(this.trump[optionOne.type] === optionTwo.type)
 		return optionOne;
 	
 	return optionTwo;
