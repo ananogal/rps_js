fbd04de7e1e038e93fd13bc60238bfc863961ac1
Sandrine <sandrine.zhangferron@gmail.com>
2014-10-20 16:06:05 +0100

    Added data type to gestures

---
+++ src/web.js
@@ -0,0 +1,5 @@
+$('#scissors').data('type', 'Scissors');
+$('#paper').data('type', 'Paper');
+$('#rock').data('type', 'Rock');
+$('#lizard').data('type', 'Lizard');
+$('#spock').data('type', 'Spock');
