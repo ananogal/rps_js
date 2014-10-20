664a54b74fb017c1d60a8054b04cf173c62b0452
Sandrine <sandrine.zhangferron@gmail.com>
2014-10-20 17:42:46 +0100

    Added game vs computer

--- src/web.js
+++ src/web.js
@@ -3,3 +3,36 @@ $('#paper').data('type', 'Paper');
 $('#rock').data('type', 'Rock');
 $('#lizard').data('type', 'Lizard');
 $('#spock').data('type', 'Spock');
+
+
+
+$('a').each(function() {
+	console.log(this);
+
+	$(this).on("click", function(){
+
+	var playerChoice = $(this).children();
+
+	var gestures = $('img').toArray();
+	var computerChoiceIndex = Math.floor(Math.random() * (gestures.length - 1));
+	var computerChoiceProxy = gestures[computerChoiceIndex];
+	var computerChoice = $(computerChoiceProxy).toArray();
+	console.log(computerChoice);
+
+	var rules = new Rules();
+	
+	var winner = rules.decidesWinner(playerChoice.data(), $(computerChoice).data());
+
+$('#msg').text('');
+$('#playerchoices').text('You played : ' + $(playerChoice).data('type') +  ' vs. Computer : ' + $(computerChoice).data('type'));
+if(winner.type === 'Draw') {
+	$('#result').text("It's a DRAW !!!");
+}
+else{
+	$('#result').text('And the winner is ' + winner.type + '!!!');
+}
+});
+});
+
+
+
