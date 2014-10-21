02f08658c4ac7423d3c998dc6e8c6f3c88949c27
ananogal <ana.nogal@gmail.com>
2014-10-21 15:28:02 +0100

    Deleted Rock,paper, scissores in javascript directory. Change the random to have the length of the gestures array

--- src/web.js
+++ src/web.js
@@ -7,39 +7,37 @@ $('#spock').data('type', 'Spock');
 
 
 $('a').each(function() {
-	console.log(this);
-
 	$(this).on("click", function(){
 
-	var playerChoice = $(this).children();
+		var playerChoice = $(this).children();
 
-	var gestures = $('img').toArray();
-	var computerChoiceIndex = Math.floor(Math.random() * (gestures.length - 1));
-	var computerChoiceProxy = gestures[computerChoiceIndex];
-	var computerChoice = $(computerChoiceProxy).toArray();
-	console.log(computerChoice);
+		var gestures = $('img').toArray();
+		var computerChoiceIndex = Math.floor(Math.random() * (gestures.length));
+		var computerChoiceProxy = gestures[computerChoiceIndex];
+		var computerChoice = $(computerChoiceProxy).toArray();
+		console.log(computerChoice);
 
-	var rules = new Rules();
-	
-	var winner = rules.decidesWinner(playerChoice.data(), $(computerChoice).data());
+		var rules = new Rules();
+		
+		var winner = rules.decidesWinner(playerChoice.data(), $(computerChoice).data());
 
-$('#msg').text('');
-$('#playerchoices').text('You played ' + $(playerChoice).data('type') +  ' --------- vs ---------  ' + $(computerChoice).data('type'));
-	var winningPlayer;
-	if(winner.type === playerChoice.data('type')){
-		winningPlayer = 'YOU'
-	}
-	else{
-		winningPlayer = 'the COMPUTER'
-	};
+		$('#msg').text('');
+		$('#playerchoices').text('You played ' + $(playerChoice).data('type') +  ' --------- vs ---------  ' + $(computerChoice).data('type'));
+		var winningPlayer;
+		if(winner.type === playerChoice.data('type')){
+			winningPlayer = 'YOU'
+		}
+		else{
+			winningPlayer = 'the COMPUTER'
+		};
 
-if(winner.type === 'Draw') {
-	$('#result').text("It's a DRAW !!!");
-}
-else{
-	$('#result').text('And the winner is... ' + winningPlayer + '!!!');
-}
-});
+		if(winner.type === 'Draw') {
+			$('#result').text("It's a DRAW !!!");
+		}
+		else{
+			$('#result').text('And the winner is... ' + winningPlayer + '!!!');
+		}
+	});
 });
 
 
